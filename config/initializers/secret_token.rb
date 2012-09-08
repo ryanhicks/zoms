40e85ed2bf7579be3f7af12ef376654abe4cab89
Ryan Hicks <ryan.c.hicks@gmail.com>
2012-09-08 16:47:59 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zoms::Application.config.secret_token = '6451877dea9bc441df7b65ca596821200441e66f11c83f4f45d530da914e6fb44e54513ef373d6c105991f223f298388ff6405417959aa6bfaa4e43d2b745323'
